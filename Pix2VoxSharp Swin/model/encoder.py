f0f8713816d8bb62742e84c1df5a637d3930caac
MahmoudSayed216 <mahmoud_das@outlook.com>
2025-03-24 08:09:05 +0200

    trying swin backbone

--- Pix2VoxSharp Swin/model/encoder.py
+++ Pix2VoxSharp Swin/model/encoder.py
@@ -6,23 +6,24 @@ from torchvision.models import convnext_base
 class Encoder(nn.Module):
     def __init__(self, configs):
         super().__init__()
-        self.backbone = self._prep_convnext(configs["model"]["pretrained"])
+        self.backbone = self._prep_swin(configs["model"]["pretrained"])
 
         
-    def _prep_convnext(self, pretrained):
+    def _prep_swin(self, pretrained):
         model_name = None if pretrained == "None" else pretrained
-        convnext_base = timm.create_model(model_name, pretrained=True, features_only = True)
+        swin_base = timm.create_model(model_name, pretrained=False, features_only = True)
         #Unfreezing all the layers
-        for param in convnext_base.parameters():
+        for param in swin_base.parameters():
             param.requires_grad = False
         #Except for the last stage
-        for param in convnext_base.stages_3.parameters():
+        for param in swin_base.layers_3.parameters():
             param.requires_grad = True
-        return convnext_base
+        return swin_base
 
 
     def forward(self, image: torch.Tensor):
         # images = images.permute(1, 0, 2, 3, 4).contiguous()
         features = self.backbone(image)
         print(features[-1].shape)
-        return features[-1]
+        
+        return features[-1].permute(0, 3, 1, 2)
