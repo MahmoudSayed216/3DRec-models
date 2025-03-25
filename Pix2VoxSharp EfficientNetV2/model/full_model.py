b4cec66f8134226a696e8a1bf6a326bad2e735d0
MahmoudSayed216 <mahmoud_das@outlook.com>
2025-03-25 07:19:15 +0200

    ENETV2

---
+++ Pix2VoxSharp EfficientNetV2/model/full_model.py
@@ -0,0 +1,19 @@
+import torch
+import torch.nn as nn
+from .encoder import Encoder
+from .decoder import Decoder
+
+
+class Pix2VoxSharp(nn.Module):
+    def __init__(self, configs):
+        super(Pix2VoxSharp, self).__init__()
+        self.encoder = Encoder(configs)
+        self.decoder = Decoder(configs)
+
+    def forward(self, x:torch.Tensor):
+        batch_size = x.size(0)
+        feature_maps = self.encoder(x)
+        upprojection = feature_maps.contiguous().view((batch_size, 3163, 2, 2, 2))
+        volume = self.decoder(upprojection)
+        return volume
+        
